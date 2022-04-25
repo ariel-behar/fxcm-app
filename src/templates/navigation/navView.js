0311a4d5c29c2dda35b65781eeb0166107276ee6
Ariel Behar <ariel.behar@hotmail.com>
2022-04-25 18:14:54 +0300

    Implementing the navigation + its styles

--- src/templates/navigation/navView.js
+++ src/templates/navigation/navView.js
@@ -1,13 +1,17 @@
 import { html } from '../../../node_modules/lit-html/lit-html.js';
 
 let navTemplate = html`
-    <nav>
-        <img src="images/logo.svg" alt="FXCM logo" />
+    <nav id="top-nav" class="container">
+        <div>
+            <img src="images/logo.svg" alt="FXCM logo" />
+        </div>
         <ul>
-            <li><a href="/risk">Risk</a></li>
+            <li><a href="/risk" class="active">Risk</a></li>
             <li><a href="/dealing">Dealing</a></li>
-            <li><a href="/ibops"></a>IBOps</li>
-            <li><a href="admin"></a>Admin</li>
+            <li><a href="/ibops">IBOps</a></li>
+            <li><a href="/admin">Admin</a></li>
+
+            <li><a href="#">LOG OFF</a></li>
         </ul>
     </nav>
 `;
