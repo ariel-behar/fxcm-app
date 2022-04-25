11eb7105f92464dfc9209ed0dcb9e6861a9e1af2
Ariel Behar <ariel.behar@hotmail.com>
2022-04-25 17:23:27 +0300

    Setting up the skeleton for the navigation

--- src/templates/navigation/navView.js
+++ src/templates/navigation/navView.js
@@ -1,8 +1,15 @@
 import { html } from '../../../node_modules/lit-html/lit-html.js';
 
 let navTemplate = html`
-    <h3>Navigation</h3> 
- 
+    <nav>
+        <img src="images/logo.svg" alt="FXCM logo" />
+        <ul>
+            <li><a href="/risk">Risk</a></li>
+            <li><a href="/dealing">Dealing</a></li>
+            <li><a href="/ibops"></a>IBOps</li>
+            <li><a href="admin"></a>Admin</li>
+        </ul>
+    </nav>
 `;
 
 export function navView(ctx) {
---
+++ src/templates/views/riskView.js
@@ -0,0 +1,10 @@
+import { html } from 'lit-html';
+
+let riskViewTemplate = html`
+    <h1>Risk View</h1>
+`
+
+export default function riskView(ctx) {
+    
+    ctx.render(riskViewTemplate);
+}
